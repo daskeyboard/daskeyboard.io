c700eab531a8d1f86c37109edfd9dc066e988d02
camille <camille@metadot.com>
2018-03-15 15:28:03 -0500

    Update test Travis

---
+++ staging/docs/docs/docs/Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'jekyll', '3.3.1'
+gem 'therubyracer'
+gem 'html-proofer'
+gem 'redcarpet'
+gem "github-pages", group: :jekyll_plugins
+gem 'bundler'
--- tool/travis.sh
+++ tool/travis.sh
@@ -15,3 +15,15 @@ rake checklinks
 
 echo "Validating Read Me."
 rake proof_readme
+
+echo "Tests passed"
+
+folder="./docs"
+if [ ! -d "$folder" ]
+then 
+  echo "Creating '$folder'"
+  mkdir $folder
+fi
+
+echo "Copy staging into docs to deploy the website"
+cp -r ./staging/* ./docs/
