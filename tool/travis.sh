b72e2f3fb6b716a23f9783f86b9d7f8f6a275251
camille <camille@metadot.com>
2018-03-15 16:04:00 -0500

    Update test Travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -17,18 +17,3 @@ echo "Validating Read Me."
 rake proof_readme
 
 echo "Tests passed"
-
-folder="./docs"
-if [ ! -d "$folder" ]
-then 
-  echo "Creating '$folder'"
-  mkdir $folder
-fi
-
-echo "Copy staging into docs to deploy the website"
-cp -r ./staging/* ./docs/
-git add .
-git commit -m 'Deploy the website'
-git remote rm origin
-git remote add origin https://github.com/DasKeyboard/q-website.git
-git push origin master
