6420a4862070d352081f7e86240d8a4ba508a21b
camille <camille@metadot.com>
2018-03-15 15:34:14 -0500

    Update test Travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -27,3 +27,7 @@ fi
 
 echo "Copy staging into docs to deploy the website"
 cp -r ./staging/* ./docs/
+git add .
+git commit -m 'Deploy the website'
+git remote add origin https://github.com/DasKeyboard/q-website.git
+git push origin master
