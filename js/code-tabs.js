128c35a161ad3fb8294bd7594face6ec097c9835
AhmedLoud <ahmed.loudghiri@gmail.com>
2018-07-18 15:12:04 -0500

    #NEW added ability to remember the last server selected (cloud or local) and show it after page load

--- js/code-tabs.js
+++ js/code-tabs.js
@@ -37,7 +37,47 @@ function listenToCodingLanguageTabSelection() {
   });
 }
 
+/**
+ * Listens to the click events on the server tab selection 
+ * and remembers the last server selected (local or cloud)
+ */
+function listenToAPIServerTabSelection() {
+  $('.cloud-or-local-nav a').on('click', function (e) {
+    e.preventDefault();
+
+    // put the selected tab link classes in an array
+    const selectedTabClassList = $(this).attr('class').split(" ");
+
+    // find the last selected server (cloud-server or local-server)
+    const lastSelectedServer= selectedTabClassList.find(function (element) {
+      return element.includes('server');
+    });
+
+    // Persist in local storage so we can pre-select around the site
+    if (localStorage) {
+      localStorage.setItem('lastSelectedServer', lastSelectedServer);
+    }
+  });
+}
+
+
+/**
+ * If there is a server remembered by the app
+ * Show all the tabs linked to this server
+ */
+function showLastSelectedServer() {
+  // select the server persisting in the local storage
+  const server = localStorage.getItem('lastSelectedServer');
+
+  if (server) {
+    $('.' + server).tab('show') // Select tab by name
+  }
+}
+
 $(document).ready(function () {
   listenToCodingLanguageTabSelection();
   showLastSelectedCodingLanguage();
+
+  listenToAPIServerTabSelection();
+  showLastSelectedServer();
 });
