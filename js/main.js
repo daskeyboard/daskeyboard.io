4af26f2a5814f62301c5423aececa5c929c3b81b
AhmedLoud <ahmed.loudghiri@gmail.com>
2018-10-30 17:04:11 -0500

    added latest software links (2.0.4)

--- js/main.js
+++ js/main.js
@@ -307,14 +307,14 @@ function loadDownloadLinkDependingOnOsArchitechture() {
   if (navigator.platform.includes('Win32')) {
     if (navigator.userAgent.indexOf("WOW64") != -1 ||
       navigator.userAgent.indexOf("Win64") != -1) {
-      $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.3/Das-Keyboard-Q-x64-Setup-2.0.3.exe");
+      $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.4/Das-Keyboard-Q-x64-Setup-2.0.4.exe");
       $('#software-download-button').append(" 64 bit");
       $('#software-version-number').append(" Windows");
       return;
 
 
     } else {
-      $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.3/Das-Keyboard-Q-ia32-Setup-2.0.3.exe");
+      $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.4/Das-Keyboard-Q-ia32-Setup-2.0.4.exe");
       $('#software-download-button').append(" 32 bit");
       $('#software-version-number').append(" Windows");
       return;
@@ -322,11 +322,18 @@ function loadDownloadLinkDependingOnOsArchitechture() {
     }
   }
   if (navigator.platform.includes('MacIntel')) {
-    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.3/DasKeyboardQ-2.0.3-beta.pkg");
+    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.4/DasKeyboardQ_2.0.4-beta.pkg");
     $('#software-download-button').append(" 64 bit");
     $('#software-version-number').append("-beta");
     $('#software-version-number').append(" Mac");
     return;
+  }
+
+  if (navigator.platform.includes('Linux x86_64')) {
+    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/2.0.4/das-keyboard-q_2.0.4~beta.deb");
+    $('#software-download-button').append(" 64 bit");
+    $('#software-version-number').append("-beta");
+    $('#software-version-number').append(" Linux");
   }
 
   $('#software-version-number').append(" Windows");
