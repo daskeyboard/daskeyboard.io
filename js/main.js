cee5ec5b48cb1788ca1a5a3dc6f9cc4e2a7e75fd
ghennion <33560687+ghennion@users.noreply.github.com>
2018-11-07 11:37:10 -0600

    updated linux  package to -beta.2

--- js/main.js
+++ js/main.js
@@ -330,7 +330,7 @@ function loadDownloadLinkDependingOnOsArchitechture() {
   }
 
   if (navigator.platform.includes('Linux x86_64')) {
-    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/3.0.0-beta/das-keyboard-q_3.0.0~beta.2.deb");
+    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/3.0.0-beta/das-keyboard-q_3.0.0-beta.2.deb");
     $('#software-download-button').append(" 64 bit");
     $('#software-version-number').append("-beta.2");
     $('#software-version-number').append(" Linux");
