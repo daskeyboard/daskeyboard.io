9d4b90072ae5e26a71892393f813e631c0a162e6
ghennion <33560687+ghennion@users.noreply.github.com>
2018-11-16 15:43:22 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -330,9 +330,9 @@ function loadDownloadLinkDependingOnOsArchitechture() {
   }
 
   if (navigator.platform.includes('Linux x86_64')) {
-    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/3.0.0-beta/das-keyboard-q_3.0.0-beta.2.deb");
+    $('#software-download-button').attr("href", "https://s3-us-west-2.amazonaws.com/q-software-releases/3.0.0-beta/das-keyboard-q_3.0.0_beta.3.deb");
     $('#software-download-button').append(" 64 bit");
-    $('#software-version-number').append("-beta.2");
+    $('#software-version-number').append("-beta.3");
     $('#software-version-number').append(" Linux");
     return;
   }
