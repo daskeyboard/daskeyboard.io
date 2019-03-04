cf1a39278efffe09451f102bbd75cd67f5a949fc
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2019-03-04 12:14:20 -0600

    Updated name.

--- js/vue-variables.js
+++ js/vue-variables.js
@@ -1,24 +1,23 @@
 var app = new Vue({
-    el: '#app',
+    el: '#videoOrGif',
     data() {
         console.log("Starting data....");
         return {
-            info: 200
+            status: 200
         }
     },
     async created() {
         console.log("Starting created....");
         try {
-            console.log("Status before: "+this.info)
+            console.log("Status before: "+this.status)
             // Using json file (spreadsheets) on google server
             const response = await axios.get(`https://spreadsheets.google.com/feeds/list/1LOzOn9s7RrDyaeNYxjIKN_rbu8DUvB3Ace3SYvM0s6Q/od6/public/basic?alt=json`)
-            this.info = response.status
-            console.log("Status after: "+this.info)
+            this.status = response.status
+            console.log("Status after: "+this.status)
         } catch (e) {
-            this.info=404
+            this.status=404
             console.log("Error: " + e)
-            console.log("Status error: "+this.info)
-
+            console.log("Status error: "+this.status)
         }
 
     }
