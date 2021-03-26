50bce674f25ec12a577166dc9cbe16dc32e92a82
fabiendv <dallavalle.fabien@gmail.com>
2021-03-26 14:22:50 +0100

    feat: added 5qs firmware instructions
    fix: added python 3.5 into pipeline
    
    fix: install new pip through pipeline
    
    feat: remove before install

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -3,6 +3,13 @@
 # Fast fail the script on failures.
 set -e
 
+# Update pip
+pip --version
+sudo apt-get install python3-pip
+alias pip=pip3
+python -m pip install -U pip
+pip --version
+
 echo "Download Google OAuth Tool"
 
 pip install --user google-oauth2l --upgrade
