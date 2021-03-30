d62d06360f9e81ee04118a05f82ad13a1cba50c6
fabiendv <dallavalle.fabien@gmail.com>
2021-03-30 16:32:27 +0200

    fix: updated pip version

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -5,5 +5,8 @@ set -e
 
 echo "Download Google OAuth Tool"
 pip --version
+/usr/bin/python -m pip install --upgrade pip
+echo "Updated pip version"
+pip --version
 
 pip install --user google-oauth2l --upgrade
