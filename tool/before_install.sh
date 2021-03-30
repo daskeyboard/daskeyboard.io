2fb558838ac5535b0d395ef6f004c10441ba848c
fabiendv <dallavalle.fabien@gmail.com>
2021-03-30 16:45:46 +0200

    chore: install pip3

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -3,10 +3,14 @@
 # Fast fail the script on failures.
 set -e
 
-echo "Download Google OAuth Tool"
-pip --version
+echo "TESTING - Update pip version"
 /usr/bin/python -m pip install --upgrade pip
 echo "Updated pip version"
 pip --version
 
-pip install --user google-oauth2l
+echo "TESTING - Install pip3"
+sudo apt-get install python3-pip
+pip3 --version
+
+echo "Download Google OAuth Tool"
+pip install --user google-oauth2l --upgrade
