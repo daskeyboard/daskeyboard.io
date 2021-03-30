747d44aea92e3b6dcd8bb7972bc96293f87df43b
fabiendv <dallavalle.fabien@gmail.com>
2021-03-30 11:03:35 +0200

    fix: back initial version

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -3,13 +3,6 @@
 # Fast fail the script on failures.
 set -e
 
-echo "PIP"
-pip3 --version
-pip install --upgrade pip
-pip --version
-pip3 install --upgrade pip
-pip3 --version
-
 echo "Download Google OAuth Tool"
 
 pip install --user google-oauth2l --upgrade
